d87df2382c794f3c7c8b8ab6c90f0ce7f0f8f170
nikkdogra <nikhildogra5769@gmail.com>
2024-02-17 17:44:56 +0530

    active links

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import { MdLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
 import { useContext } from 'react';
@@ -25,7 +25,7 @@ const Navbar = ({ onToggleMode }) => {
 
       <div className="container-fluid px-5">
 
-        <Link to='/' className="navbar-brand"><span className={`badge px-3 py-2 bg-white text-${mode === 'light' ? 'primary' : 'black'}`}>MY SHOP</span></Link>
+        <NavLink to='/' className="navbar-brand"><span className={`badge px-3 py-2 bg-white text-${mode === 'light' ? 'primary' : 'black'}`}>MY SHOP</span></NavLink>
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -34,10 +34,10 @@ const Navbar = ({ onToggleMode }) => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to='/' className="nav-link active text-white" aria-current="page">Home</Link>
+              <NavLink to='/' className={({ isActive }) => `nav-link text-white ${isActive && 'fw-bolder'}`} aria-current="page">Home</NavLink>
             </li>
             <li className="nav-item">
-              <Link to='/cart' className="nav-link text-white">Cart</Link>
+              <NavLink to='/cart' className={({ isActive }) => `nav-link text-white ${isActive && 'fw-bolder'}`}>Cart</NavLink>
             </li>
           </ul>
           <div className='d-flex flex-column-reverse flex-lg-row gap-lg-3'>
